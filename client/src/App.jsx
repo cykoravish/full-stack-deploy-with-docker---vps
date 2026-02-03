99ead805b67fcc1c1330c404b8f99536aa7708f5
cykoravish <ravishbisht85@gmail.com>
2026-02-03 06:48:01 +0530

    change server endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,8 @@ import { useState } from "react";
 function App() {
   const [message, setMessage] = useState("");
   useEffect(() => {
-    fetch("http://localhost:4000/api/message")
+    // fetch("http://localhost:4000/api/message")
+    fetch("http://72.61.227.248:4000/api/message")
       .then((res) => res.json())
       .then((data) => setMessage(data.message))
       .catch(err=>console.log("error: ", err))
